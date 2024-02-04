d9dd78d9596f87add9afe5bec2a69797585e70e0
devinshade <devindanielle93@gmail.com>
2024-02-04 15:30:02 -0700

    updated reaction model and added thought model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,1 +1,34 @@
-// Schema only
+const { Schema, Types } = require('mongoose');
+const dateFormat = require("../utils/dateFormat");
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        // Use a getter method to format the timestamp on query
+            get: (timestamp) => dateFormat(timestamp),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = reactionSchema;
